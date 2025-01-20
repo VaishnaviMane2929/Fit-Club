74d5b7bd05ae864b615333652cd8ad10ca3b55c8
vaishnavi <vaishnavimane991@gmail.com>
2025-01-20 23:12:56 +0530

    uncomlpeted testimonials class

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import '../Testimonials/Testimonials.css'
 import {testimonialsData} from "../data/testimonialsData"
-
+import leftArrow from '../../assets/leftArrow.png'
+import rightArrow from '../../assets/rightArrow.png'
 const Testimonials = () => {
   const [selected, setSelected] = useState(0);
   const tLength =testimonialsData.length;
@@ -15,8 +16,24 @@ const Testimonials = () => {
                 <span>
                   {testimonialsData[selected].review}
                 </span>
+                <span>
+                  <span style={{color: 'var(--orange)'}}>
+                    {testimonialsData[selected].name}
+                  </span>{" "}
+                  - {testimonialsData[selected].status}
+                </span>
             </div>
-            <div className='right-t'></div>
+            <div className='right-t'>
+              <div></div>
+              <div></div>
+              <img src={testimonialsData[selected].image} alt="" />
+
+              <div className='arrows'>
+                <img src={leftArrow} alt="" />
+                <img src={rightArrow} alt="" />
+
+              </div>
+            </div>
 
        
         
